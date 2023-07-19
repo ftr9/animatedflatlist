a8bb6cd4f75316c59e0ad703398859cb7b70f1a8
ftr9 <ferlandodotel16@gmail.com>
2023-07-19 21:55:24 +0545

    refactor: Moved FoodSlider up to Components

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, FlatList, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Layout/Header';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import FoodCard from './components/Cards/FoodCard';
-import HomeSectionOne from './components/section/HomeSectionOne';
 
-import Animated, {
-  useSharedValue,
-  useAnimatedScrollHandler,
-} from 'react-native-reanimated';
+import HomeSectionOne from './components/section/HomeSectionOne';
+import FoodSlider from './components/Layout/FoodSlider';
 
 const SafeArea = ({ children }) => {
   return (
@@ -22,87 +17,6 @@ const SafeArea = ({ children }) => {
         {children}
       </SafeAreaView>
     </SafeAreaProvider>
-  );
-};
-
-const FoodSlider = () => {
-  const sharedVal = useSharedValue(0);
-
-  const onScrollHandle = useAnimatedScrollHandler(e => {
-    sharedVal.value = e.contentOffset.x;
-  });
-
-  const data = [1, 2, 3, 4, 5, 6];
-
-  const calculateSnapOffsets = () => {
-    const cardWidth = (80 / 100) * Dimensions.get('screen').width + 10;
-    const values = data.map(el => {
-      if (el === 1) {
-        return [0, cardWidth];
-      }
-
-      return [cardWidth * (el - 2), cardWidth * (el - 1), cardWidth * el];
-    });
-
-    return values;
-  };
-  const calculatedSnapOffsets = calculateSnapOffsets();
-  const ListDatas = [
-    {
-      image:
-        'https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-  ];
-
-  return (
-    <Animated.FlatList
-      style={{
-        marginTop: 40,
-      }}
-      data={data}
-      horizontal
-      onScroll={onScrollHandle}
-      snapToOffsets={calculatedSnapOffsets.flat(1)}
-      decelerationRate={0.05}
-      snapToAlignment={'start'}
-      renderItem={({ item }) => {
-        return (
-          <FoodCard
-            sharedValue={sharedVal}
-            snapOffset={calculatedSnapOffsets[item - 1]}
-            imageUrl={ListDatas[item - 1].image}
-          />
-        );
-      }}
-      ListFooterComponent={() => {
-        return (
-          <View
-            style={{ width: Dimensions.get('screen').width * (18 / 100) }}
-          ></View>
-        );
-      }}
-      keyExtractor={(_, index) => index}
-    ></Animated.FlatList>
   );
 };
 
---
+++ components/Layout/FoodSlider.js
@@ -0,0 +1,89 @@
+import { Text, View, FlatList, Dimensions } from 'react-native';
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from 'react-native-reanimated';
+import FoodCard from '../Cards/FoodCard';
+
+const FoodSlider = () => {
+  const sharedVal = useSharedValue(0);
+
+  const onScrollHandle = useAnimatedScrollHandler(e => {
+    sharedVal.value = e.contentOffset.x;
+  });
+
+  const data = [1, 2, 3, 4, 5, 6];
+
+  const calculateSnapOffsets = () => {
+    const cardWidth = (80 / 100) * Dimensions.get('screen').width + 10;
+    const values = data.map(el => {
+      if (el === 1) {
+        return [0, cardWidth];
+      }
+
+      return [cardWidth * (el - 2), cardWidth * (el - 1), cardWidth * el];
+    });
+
+    return values;
+  };
+  const calculatedSnapOffsets = calculateSnapOffsets();
+  const ListDatas = [
+    {
+      image:
+        'https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=400',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&w=400',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=400',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=400',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=400',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&w=400',
+    },
+  ];
+
+  return (
+    <Animated.FlatList
+      style={{
+        marginTop: 40,
+      }}
+      data={data}
+      horizontal
+      onScroll={onScrollHandle}
+      snapToOffsets={calculatedSnapOffsets.flat(1)}
+      decelerationRate={0.05}
+      snapToAlignment={'start'}
+      renderItem={({ item }) => {
+        return (
+          <FoodCard
+            sharedValue={sharedVal}
+            snapOffset={calculatedSnapOffsets[item - 1]}
+            imageUrl={ListDatas[item - 1].image}
+          />
+        );
+      }}
+      ListFooterComponent={() => {
+        return (
+          <View
+            style={{ width: Dimensions.get('screen').width * (18 / 100) }}
+          ></View>
+        );
+      }}
+      keyExtractor={(_, index) => index}
+    ></Animated.FlatList>
+  );
+};
+
+export default FoodSlider;
