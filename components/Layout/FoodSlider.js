e9f42756d5c2346064b30d0e969263b115af70c0
ftr9 <ferlandodotel16@gmail.com>
2023-07-19 21:59:44 +0545

    feat: Added Sample_data and List_datas Values to Top

--- components/Layout/FoodSlider.js
+++ components/Layout/FoodSlider.js
@@ -5,6 +5,34 @@ import Animated, {
 } from 'react-native-reanimated';
 import FoodCard from '../Cards/FoodCard';
 
+const SAMPLE_DATA = [1, 2, 3, 4, 5, 6];
+const LIST_DATAS = [
+  {
+    image:
+      'https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&w=400',
+  },
+];
+
 const FoodSlider = () => {
   const sharedVal = useSharedValue(0);
 
@@ -12,11 +40,9 @@ const FoodSlider = () => {
     sharedVal.value = e.contentOffset.x;
   });
 
-  const data = [1, 2, 3, 4, 5, 6];
-
   const calculateSnapOffsets = () => {
     const cardWidth = (80 / 100) * Dimensions.get('screen').width + 10;
-    const values = data.map(el => {
+    const values = SAMPLE_DATA.map(el => {
       if (el === 1) {
         return [0, cardWidth];
       }
@@ -27,39 +53,13 @@ const FoodSlider = () => {
     return values;
   };
   const calculatedSnapOffsets = calculateSnapOffsets();
-  const ListDatas = [
-    {
-      image:
-        'https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-    {
-      image:
-        'https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&w=400',
-    },
-  ];
 
   return (
     <Animated.FlatList
       style={{
         marginTop: 40,
       }}
-      data={data}
+      data={SAMPLE_DATA}
       horizontal
       onScroll={onScrollHandle}
       snapToOffsets={calculatedSnapOffsets.flat(1)}
@@ -70,7 +70,7 @@ const FoodSlider = () => {
           <FoodCard
             sharedValue={sharedVal}
             snapOffset={calculatedSnapOffsets[item - 1]}
-            imageUrl={ListDatas[item - 1].image}
+            imageUrl={LIST_DATAS[item - 1].image}
           />
         );
       }}
